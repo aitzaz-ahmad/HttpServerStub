2a166dec2d1a55b7a28589b89ac28e7ca46ff9ee
aitzaz-ahmad <cptn.aitzaz@gmail.com>
2017-11-15 21:47:23 +0500

    another attempt at making the server reachable on heroku

--- HelloWorldHttpServer.js
+++ HelloWorldHttpServer.js
@@ -1,20 +1,21 @@
 //HelloWorld Node.js HTTP Server
 
-const http = require('http')
-const LISTENING_PORT_HTTP  = process.env.PORT || 3000
+const http = require('http');
+const LISTENING_PORT_HTTP  = process.env.PORT || 3000;
 
 const requestHandler = (request, response) => {
   console.log(request.url)
-  response.WriteHead(200);
-  response.end('Hello Node.js Server!')
-}
+  response.WriteHead(200, {'Content-Type': 'text/html'});
+  response.Write('Hello World! Node.js server here...');
+  response.end();
+};
 
-const http_server = http.createServer(requestHandler)
+const http_server = http.createServer(requestHandler);
 
 http_server.listen(LISTENING_PORT_HTTP, (err) => {
   if (err) {
-    return console.log('something bad happened', err)
+    return console.log('something bad happened', err);
   }
 
-  console.log(`server is listening on ${LISTENING_PORT_HTTP}`)
-})
+  console.log(`server is listening on ${LISTENING_PORT_HTTP}`);
+});
