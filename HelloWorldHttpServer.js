4564cd275ce33c0210c6df87a0af98017fc2cd35
aitzaz-ahmad <cptn.aitzaz@gmail.com>
2017-11-15 13:12:26 +0500

    made tweaks to the package.json file and made improvements to the http server stub as well. reverted changes pertaining to the https protocol.

--- HelloWorldHttpServer.js
+++ HelloWorldHttpServer.js
@@ -1,19 +1,23 @@
 //HelloWorld Node.js HTTP Server
 
-const http = require('https')
-const port = 3000
+const http = require('http')
+const https = require('https')
+const fStream = require('fs')
+const LISTENING_PORT_HTTP  = 3000
+const LISTENING_PORT_HTTPS = 8000
 
 const requestHandler = (request, response) => {
   console.log(request.url)
+  response.WriteHead(200);
   response.end('Hello Node.js Server!')
 }
 
-const server = http.createServer(requestHandler)
+const http_server = http.createServer(requestHandler)
 
-server.listen(port, (err) => {
+http_server.listen(LISTENING_PORT_HTTP, (err) => {
   if (err) {
     return console.log('something bad happened', err)
   }
 
-  console.log(`server is listening on ${port}`)
+  console.log(`server is listening on ${LISTENING_PORT_HTTP}`)
 })
